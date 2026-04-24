771b832aede3e66a4799c9fd6d8e413b3b125f3e
B1TAMP <damianhoferica@gmail.com>
2026-04-24 22:39:32 +0200

    hptovoooo

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import sys
 from PyQt6.QtWidgets import QApplication
 from app import DoublePendulumApp
 
+ # Parametre kyvadla a simulácie 
+  # l1  = 125
+   # l2 = 85
+    # m1 = 
+     # m2 = 
+      # potrebujem upravit toolbary l1 a l2  momentalne v metroch idealne ale od 3cm do 1m .
+
 
 def main():
     app = QApplication(sys.argv)
